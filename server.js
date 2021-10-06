12407820731d208ee655b5ade7742049fe61d2dc
Umer Inayat <umerinayat.369@gmail.com>
2021-10-06 22:42:07 +0500

    basic node js websocket server and client app

---
+++ server.js
@@ -0,0 +1,12 @@
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({ port: 8181 });
+
+wss.on('connection', (ws) => {
+    console.log('client connected!');
+
+    ws.on('message', (message) => {
+        console.log('message');
+    });
+
+});
